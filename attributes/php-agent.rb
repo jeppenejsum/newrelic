1f6c2e6651845bf6f0e648fdbf6093cdeae8c344
Jeppe Nejsum Madsen <jeppe@ingolfs.dk>
2014-03-23 22:53:48 +0100

    Fix

--- attributes/php-agent.rb
+++ attributes/php-agent.rb
@@ -4,7 +4,7 @@
 #
 # Copyright 2012-2014, Escape Studios
 #
-ext_conf_dir = node['php'].nil? "" : node['php']['ext_conf_dir']
+ext_conf_dir = node['php'].nil? ? "" : node['php']['ext_conf_dir']
 default['newrelic']['php-agent']['startup_mode'] = "agent"
 default['newrelic']['php-agent']['web_server']['service_name'] = "apache2"
 default['newrelic']['php-agent']['php_recipe'] = "php::default"
